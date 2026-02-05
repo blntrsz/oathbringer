fe57f43a2f35b7f00f18f18649382080079d8b29
Balint Orosz <orosz.balint.97@gmail.com>
2026-02-05 20:01:16 +0100

    refactor setup into functions

--- setup.sh
+++ setup.sh
@@ -1,10 +1,25 @@
 #! /bin/sh
 
-apt update -y &&
-apt upgrade -y &&
-sudo apt install software-properties-common &&
-sudo add-apt-repository --yes --update ppa:ansible/ansible &&
-apt install -y git ansible &&
-[ ! -d "oathbringer" ] && git clone https://github.com/blntrsz/oathbringer.git &&
-ansible-playbook ~/oathbringer/local.yaml --ask-become-pass
+base_setup() {
+  apt update -y &&
+  apt upgrade -y
+}
 
+install() {
+  sudo apt install software-properties-common &&
+  sudo add-apt-repository --yes --update ppa:ansible/ansible &&
+  apt install -y git ansible
+}
+
+clone_repo() {
+  rm -rf ~/oathbringer && git clone https://github.com/blntrsz/oathbringer.git
+}
+
+ansible_run() {
+  ansible-playbook ~/oathbringer/local.yaml --ask-become-pass
+}
+
+base_setup &&
+install &&
+clone_repo &&
+ansible_run
