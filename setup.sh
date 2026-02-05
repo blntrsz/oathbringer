76f3a668dc172b5e5f0f657b75b80403389f9ded
Balint Orosz <orosz.balint.97@gmail.com>
2026-02-05 19:56:10 +0100

    add repo check

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ apt upgrade -y &&
 sudo apt install software-properties-common &&
 sudo add-apt-repository --yes --update ppa:ansible/ansible &&
 apt install -y git ansible &&
-git clone https://github.com/blntrsz/oathbringer.git &&
+[ ! -d "oathbringer" ] && git clone https://github.com/blntrsz/oathbringer.git &&
 ansible-playbook ~/oathbringer/local.yaml --ask-become-pass
 
