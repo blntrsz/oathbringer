cd849c17bc8646ea12024d556496d0d16634324c
Balint Orosz <orosz.balint.97@gmail.com>
2026-02-05 19:41:10 +0100

    initial setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# OathBridnger server setup
+
+```bash
+curl -fsSL https://raw.githubusercontent.com/blntrsz/oathbringer/refs/heads/main/setup.sh | bash
+```
---
+++ setup.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+apt update -y &&
+apt upgrade -y &&
+sudo apt install software-properties-common &&
+sudo add-apt-repository --yes --update ppa:ansible/ansible &&
+apt install -y git ansible &&
+git clone https://github.com/blntrsz/oathbringer.git &&
+ansible-playbook ~/oathbringer/local.yml --ask-become-pass
+
